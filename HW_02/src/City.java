b877eee255aa87105c878634989ec560f7f974d4
Peter Tucker <ptucker@whitworth.edu>
2019-01-09 11:27:31 -0800

    feedback

--- HW_02/src/City.java
+++ HW_02/src/City.java
@@ -1,3 +1,7 @@
+//PT -- javadoc. -2
+
+//PT -- I can't really do anything with this class. Get/set functions? Add people and buildings? -4
+
 public class City {
     private Building[] buildings;
     private Person[] people;
