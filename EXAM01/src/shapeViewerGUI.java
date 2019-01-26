6218615bfbec91eeffde5e4dd651098b86720ac1
Peter Tucker <ptucker@whitworth.edu>
2019-01-26 15:10:09 -0800

    Update shapeViewerGUI.java

--- EXAM01/src/shapeViewerGUI.java
+++ EXAM01/src/shapeViewerGUI.java
@@ -1,3 +1,19 @@
+/*
+FEEDBACK
+
+Read from the file correctly: 20/20%
+Implement class hierarchy correctly: 20/20%
+Populate appropriate data structures with shape data: 15/15%
+Show list of shapes correctly: 15/15%
+Show specific shape properties correctly: 14/15%
+Well-structured, well-commented code: 15/15%
+
+
+Comments:
+  Images not appearing
+  Shape doesn't need to define toString, since it's on Object
+*/
+
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
